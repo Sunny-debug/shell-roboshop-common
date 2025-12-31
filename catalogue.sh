65b61d36dd4beee82135b8951925335a0219bca6
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-31 11:33:09 -0600

    Practice

--- catalogue.sh
+++ catalogue.sh
@@ -13,7 +13,7 @@ VALIDATE $? "Copying Mongo Repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongo Client"
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.dawgs.online --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
