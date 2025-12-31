b08c980e16980a0b1efaf1011a1e1fc63572d9c4
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-31 11:02:04 -0600

    Practice

--- common.sh
+++ common.sh
@@ -8,8 +8,10 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.dawgs.online
 mkdir -p $LOGS_FOLDER
 echo "Script execution started at: $(date)" | tee -a $LOG_FILE
 
