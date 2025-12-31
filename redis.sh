0188e06231cbda6703278a4be03ca4587e20e5bb
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-31 12:19:00 -0600

    Practice

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ dnf module disable $app_name -y &>>$LOG_FILE
 dnf module enable redis:7 -y &>>$LOG_FILE
 
 dnf install $app_name -y &>>$LOG_FILE
-VALIDATE $? '$app_name Installed'
+VALIDATE $? ' Redis Installed'
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 sed -i 's/^protected-mode[[:space:]]\+yes/protected-mode no/' /etc/redis/redis.conf
@@ -17,6 +17,6 @@ sed -i 's/^protected-mode[[:space:]]\+yes/protected-mode no/' /etc/redis/redis.c
 systemctl enable redis &>>$LOG_FILE
 
 systemctl start $app_name &>>$LOG_FILE
-VALIDATE $? ' Successfully Installed $app_name '
+VALIDATE $? ' Successfully Installed '
 
 print_total_time
